c7d7116d1429296c9eace6eca4e73794a7ba6d1c
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-07 12:12:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,32 +9,31 @@
 #============================================================
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.8/g' package/base-files/files/bin/config_generate
+ 
+# 修改hostname
+sed -i 's/OpenWrt/XinV-2.0/g' package/base-files/files/bin/config_generate
+ 
+# Modify the version number版本号里显示一个自己的名字（AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+sed -i 's/OpenWrt /Build $(TZ=UTC-8 date "+%Y.%m.%d") @ XinV 2.0 /g' package/lean/default-settings/files/zzz-default-settings
+ 
+# 修改主机名字，把XinV-2.0修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='XinV-2.0'' package/lean/default-settings/files/zzz-default-settings
 
-# 删除默认密码
-# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+# 拉取 darkmatter/luci-theme-jj 原作者的源码
+git clone https://github.com/netitgo/luci-theme-jj package/uci-theme-jj
+git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
+git clone https://github.com/coLATin/argon-mod.git package/argon-mod
 
-# 修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
 
-# 取消bootstrap为默认主题
-# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 修改系统主机名
-# sed -i 's/OpenWrt/HIWIFI_B70/g' package/base-files/files/bin/config_generate
-
-# 修改默认wifi名称ssid为tymishop
-# sed -i 's/ssid=OpenWrt/ssid=tymishop/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 替换默认主题为 luci-theme-neobird
+sed -i 's/luci-theme-bootstrap/luci-theme-darkmatter/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/更改前的信息/更改后的信息/g' ./要修改的文件的目录（可以用本地查看）
 
 #开启MU-MIMO
-# sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-#wifi加密方式，没有是none
-# sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-#wifi密码
-# sed -i 's/key=15581822425/key=gds.2021/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 删除软件包
 rm -rf package/lean/luci-theme-argon
-
